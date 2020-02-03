afdae284e3001a06ab6adb086c4f9572bc50e5aa
Jonathan <jmedel.7@gmail.com>
2020-02-03 11:51:39 -0300

    Create seed and form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.destroy_all
+
+Todo.create(
+    completed: true,
+    description: 'Instalar versión de ruby'
+)
+
+Todo.create(
+    completed: true,
+    description: 'Instalar versión de rails'
+)
+
+Todo.create(
+    completed: true,
+    description: 'Settear un gemset'
+)
+
+Todo.create(
+    completed: true,
+    description: 'Crear nuevo proyecto'
+)
+
+Todo.create(
+    completed: true,
+    description: 'Generar el modelo'
+)
+
+Todo.create(
+    completed: true,
+    description: 'Ejecutar la migración'
+)
+
+Todo.create(
+    completed: false,
+    description: 'Crear rutas'
+)
+
+Todo.create(
+    completed: false,
+    description: 'Crear vista'
+)
+
+Todo.create(
+    completed: false,
+    description: 'Generar controlador'
+)
+
+Todo.create(
+    completed: false,
+    description: 'Generar modelo'
+)
