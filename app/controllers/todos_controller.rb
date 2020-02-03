889f412ff5e8a5f8ea61a6156936fedb30eeb5ab
Jonathan <jmedel.7@gmail.com>
2020-02-03 14:42:24 -0300

    fin de segunda parte

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+    before_action :set_todo, only: [:edit, :show, :update, :destroy]
+    
     def index
     @todos = Todo.all
     end
@@ -6,4 +8,41 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end
+
+    def create 
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            redirect_to todos_path
+        else
+            render :new
+                
+        end
+    end
+
+    def show 
+        @todo = Todo.find(params[:id])
+    end
+
+    def edit 
+    end
+
+    def update
+        @todo.update(todo_params)
+        redirect_to todos_path, notice: 'El registro ha muerto.'
+    end
+
+    def destroy
+        @todo.destroy
+        redirect_to todos_path
+    end
+
+
+    private
+    def todo_params 
+    params.require(:todo).permit(:completed, :description)
+    end
+    def set_todo
+        @todo = Todo.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   get 'todos/new', as: 'new_todo'
-  
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'show_todos'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  patch 'todos/:id', to: 'todos#update', as: 'todo'
+  delete 'todos/:id', to: 'todos#destroy', as: 'delete_todo'
   root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
